7970ee481c00a62b20989cefd700022587e1793e
Yeabsira Solomon <111006957+yab-sol@users.noreply.github.com>
2023-01-04 21:58:24 +0300

    Add files via upload

---
+++ 2023_01_03_201417_create_tmpreserve_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTmpreserveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tmpreserve', function (Blueprint $table) {
+            $table->id();
+            $table->date('departuredate');
+            $table->unsignedBigInteger('userid');
+            $table->unsignedBigInteger('packageid');
+            $table->unsignedBigInteger('tourid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tmpreserve');
+    }
+}
