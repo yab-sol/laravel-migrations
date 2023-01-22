6ad152b41544924b1e38eedf40b40d83d7f5bf57
Yeabsira Solomon <111006957+yab-sol@users.noreply.github.com>
2023-01-22 10:48:26 +0300

    update status column

--- 2014_10_12_000000_create_users_table.php
+++ 2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('profilepath')->nullable();
-            $table->integer('status');
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
 
