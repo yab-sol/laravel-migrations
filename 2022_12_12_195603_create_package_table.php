57be3dec82a4da4111a6b1c4a91003246e8c5f92
Yeabsira Solomon <111006957+yab-sol@users.noreply.github.com>
2023-01-22 10:54:19 +0300

    Update status

--- 2022_12_12_195603_create_package_table.php
+++ 2022_12_12_195603_create_package_table.php
@@ -27,7 +27,7 @@ class CreatePackageTable extends Migration
             $table->string('duration');
             $table->text('operatedin')->nullable();
             $table->unsignedBigInteger('tourid');
-            $table->integer('status');
+            $table->integer('status')->default(1);
             $table->foreign('tourid')->references('id')->on('touroperator');
             $table->timestamps();
         });
